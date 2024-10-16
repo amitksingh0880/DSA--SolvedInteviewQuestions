18ebf541734e0bcc94d1ee7f00026c580c0ab3f3
Amit Kumar Singh <150078011+amitksingh0880@users.noreply.github.com>
2024-10-16 11:53:24 +0530

    Time: 93 ms (5.03%), Space: 44.9 MB (47.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
           
-        // for(int i=0;i<nums.length-1;i++)
-        // {
-        //     for(int j=i+1;j<nums.length;j++)
-        //     {
-        //         int sum=0;
-        //         sum=nums[i]+nums[j];
-        //         if(sum==target)
-        //         {
-        //          return new int[]{i,j};
-        //         }
-        //     }
-        // } 
-        
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j= 0 ;j<nums.length;j++)
+            {
+                // int sum=0;
+                // sum=;
+                if(nums[i]+nums[j]==target && i != j)
+                {
+                 return new int[]{i,j};
+                }
+            }
+        } 
+     return new int[]{0,0};
+    }    
     //     HashMap<Integer,Integer> Map = new HashMap<>();
     //        int A,B;
     //     for(int i = 0 ; i < nums.length ; i++)
@@ -30,18 +31,18 @@ class Solution {
     //     }
          
     //   return null;
-       HashMap<Integer,Integer> map = new HashMap<>();
-        int sIndex;
-        int eIndex;
-       for(int i = 0 ; i < nums.length ; i++){
-            if(map.containsKey(target - nums[i])){
-                sIndex = i;
-                eIndex = map.get(target - nums[i]);
-                return new int[]{sIndex, eIndex};
-            }else{
-                map.put(nums[i],i);
-            }
-       }
-     return new int[]{0,0};
-    }
+    //    HashMap<Integer,Integer> map = new HashMap<>();
+    //     int sIndex;
+    //     int eIndex;
+    //    for(int i = 0 ; i < nums.length ; i++){
+    //         if(map.containsKey(target - nums[i])){
+    //             sIndex = i;
+    //             eIndex = map.get(target - nums[i]);
+    //             return new int[]{sIndex, eIndex};
+    //         }else{
+    //             map.put(nums[i],i);
+    //         }
+    //    }
+    //  return new int[]{0,0};
+    // }
 }
